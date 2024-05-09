7d83a8ced209138130b6a66931148238382f91f1
Picciotto <mirko.picciotto@claritergroup.com>
2024-05-09 15:37:54 +0200

    Cinema, accessibility, seats

--- js/cinema.js
+++ js/cinema.js
@@ -3,8 +3,7 @@ document.getElementById('submit').addEventListener('click', function() {
     checkboxes.forEach(function(checkbox) {
         if (checkbox.checked && !checkbox.disabled) {
             checkbox.disabled = true; // Disable the checkbox
-            checkbox.parentElement.classList.add('selected'); // Add 'selected' for green color
-            checkbox.parentElement.classList.add('not_available'); // Add 'not_available' to prevent further selection
+            checkbox.parentElement.classList.add('reserved'); // Add 'not_available' to prevent further selection
         }
     });
     alert("I posti scelti sono stati prenotati");
@@ -14,7 +13,5 @@ document.getElementById('cancel').addEventListener('click', function() {
     const checkboxes = document.querySelectorAll('.seat input[type="checkbox"]');
     checkboxes.forEach(function(checkbox) {
         checkbox.checked = false; // Deselect all checkboxes
-        checkbox.parentElement.classList.remove('selected', 'not_available'); // Remove 'selected' and 'not_available' classes
-        checkbox.disabled = false; // Re-enable the checkbox for future selection
     });
 });
