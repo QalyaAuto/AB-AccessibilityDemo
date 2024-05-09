553750b07e84be6074bb96d381ee063cf51e7658
Picciotto <mirko.picciotto@claritergroup.com>
2024-05-09 17:51:53 +0200

    Cinema completed

--- js/cinema.js
+++ js/cinema.js
@@ -1,12 +1,24 @@
 document.getElementById('submit').addEventListener('click', function() {
     const checkboxes = document.querySelectorAll('.seat input[type="checkbox"]');
+    const reservedSeats = document.querySelector('#reservedSeats');
+
+    let checkedSeats = [];
+
     checkboxes.forEach(function(checkbox) {
         if (checkbox.checked && !checkbox.disabled) {
+            checkedSeats.push(checkbox.id)
             checkbox.disabled = true; // Disable the checkbox
             checkbox.parentElement.classList.add('reserved'); // Add 'not_available' to prevent further selection
         }
     });
-    alert("I posti scelti sono stati prenotati");
+
+    if(checkedSeats.length > 0 ){
+        let msg = 'I posti '+checkedSeats.join(', ')+' sono stati prenotati'
+        reservedSeats.innerHTML = "Avete prenotato i posti "+checkedSeats.join(', ')
+        reservedSeats.style.display = "block"
+        alert(msg);
+    }
+    
 });
 
 document.getElementById('cancel').addEventListener('click', function() {
